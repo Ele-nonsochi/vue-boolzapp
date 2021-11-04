fe9cd619143c0d6443b402f5a6a1e11750d8bcae
Ele-nonsochi <elea.luzi25@gmail.com>
2021-11-04 15:20:50 +0100

    Inizio lavoro Java

--- js/script1.js
+++ js/script1.js
@@ -1,8 +1,36 @@
 Vue.config.devtools = true
 
 new Vue({
-    el: "root",
+    el: "#root",
     data: {
+        contacts: [
+            {
+                name: "Giorgio",
+                avatar: "img/avatar_1.jpg",
+                messages: "ciao",
+                lastdate: "23/09/2021",
+            },
+            {
+                name: "David",
+                avatar: "img/avatar_2.jpg",
+                messages: "ciao",
+                lastdate: "23/09/2021",
+            },
+            {
+                name: "Jack",
+                avatar: "img/avatar_3.jpg",
+                messages: "ciao",
+                lastdate: "23/09/2021",
+            },
+            {
+                name: "Emanuele",
+                avatar: "img/avatar_4.jpg",
+                messages: "ciao",
+                lastdate: "23/09/2021",
+            },
+
+        ],
+        newMessage: ""
 
     }
-})
+});
